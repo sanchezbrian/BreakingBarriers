67bb5a4070f5807fd53f5bf3ee067a84ce17ebfd
Brian Sanchez <brian36@fb.com>
2020-07-15 11:57:11 -0700

    added pods and conversation view controller

---
+++ BreakingBarriers/View Controller/ConversationViewController.m
@@ -0,0 +1,32 @@
+//
+//  ConversationViewController.m
+//  BreakingBarriers
+//
+//  Created by Brian Sanchez on 7/15/20.
+//  Copyright © 2020 Brian Sanchez. All rights reserved.
+//
+
+#import "ConversationViewController.h"
+
+@interface ConversationViewController ()
+
+@end
+
+@implementation ConversationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
