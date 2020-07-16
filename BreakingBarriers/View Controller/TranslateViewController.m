4895d29fdd9845078c624b26f2ce4bd92e2e8126
Brian Sanchez <brian36@fb.com>
2020-07-16 12:24:44 -0700

    user is able to change between languages in translation view

--- BreakingBarriers/View Controller/TranslateViewController.m
+++ BreakingBarriers/View Controller/TranslateViewController.m
@@ -7,13 +7,17 @@
 //
 
 #import "TranslateViewController.h"
+#import "LanguageChooserViewController.h"
 @import MLKit;
 
-@interface TranslateViewController ()
+@interface TranslateViewController () <LanguageChooserViewControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIButton *langOneButton;
 @property (weak, nonatomic) IBOutlet UIButton *langTwoButton;
 @property (weak, nonatomic) IBOutlet UITextField *sourceTextField;
 @property (weak, nonatomic) IBOutlet UILabel *outputLabel;
+
+@property (strong, nonatomic) NSString *langOne;
+@property (strong, nonatomic) NSString *langTwo;
 
 @end
 
@@ -23,15 +27,39 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+- (IBAction)changeLangOne:(id)sender {
+    [self performSegueWithIdentifier:@"languagePicker" sender:sender];
+}
+- (IBAction)changeLangTwo:(id)sender {
+    [self performSegueWithIdentifier:@"languagePicker" sender:sender];
+}
 
-/*
+- (void)languageChooserViewController:(LanguageChooserViewController *)contoller didPickLanguage:(NSString *)language {
+    if (contoller.langOne) {
+        self.langOne = language;
+        [self.langOneButton setTitle:[NSLocale.currentLocale localizedStringForLanguageCode:language] forState:UIControlStateNormal];
+        NSLog(@"Language 1: %@", language);
+    } else {
+        self.langTwo = language;
+        [self.langTwoButton setTitle:[NSLocale.currentLocale localizedStringForLanguageCode:language] forState:UIControlStateNormal];
+        NSLog(@"Language 2: %@", language);
+    }
+}
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    UINavigationController *navController = segue.destinationViewController;
+    LanguageChooserViewController *controller = navController.topViewController;
+    controller.delegate = self;
+    if ([sender tag] == 1) {
+        controller.langOne = YES;
+    } else {
+        controller.langTwo = YES;
+    }
 }
-*/
 
 @end
