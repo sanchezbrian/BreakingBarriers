c6e7b7b18f4a571ccc6a34305c560d775ecd470e
Brian Sanchez <brian36@fb.com>
2020-07-17 10:45:33 -0700

    added initial scan view controller with live camera view

---
+++ BreakingBarriers/View Controller/ScanViewController.h
@@ -0,0 +1,17 @@
+//
+//  ScanViewController.h
+//  BreakingBarriers
+//
+//  Created by Brian Sanchez on 7/17/20.
+//  Copyright © 2020 Brian Sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ScanViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BreakingBarriers/View Controller/ScanViewController.m
@@ -0,0 +1,83 @@
+//
+//  ScanViewController.m
+//  BreakingBarriers
+//
+//  Created by Brian Sanchez on 7/17/20.
+//  Copyright © 2020 Brian Sanchez. All rights reserved.
+//
+
+#import "ScanViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface ScanViewController ()
+@property (weak, nonatomic) IBOutlet UIView *previewView;
+
+@property (nonatomic) AVCaptureSession *captureSession;
+@property (nonatomic) AVCapturePhotoOutput *stillImageOutput;
+@property (nonatomic) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+@end
+
+@implementation ScanViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    self.captureSession = [AVCaptureSession new];
+    self.captureSession.sessionPreset = AVCaptureSessionPresetPhoto;
+    AVCaptureDevice *backCamera = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if (!backCamera) {
+        NSLog(@"Unable to access back camera");
+        return;
+    }
+    NSError *error;
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:backCamera error:&error];
+    if (!error) {
+        self.stillImageOutput = [AVCapturePhotoOutput new];
+
+        if ([self.captureSession canAddInput:input] && [self.captureSession canAddOutput:self.stillImageOutput]) {
+            
+            [self.captureSession addInput:input];
+            [self.captureSession addOutput:self.stillImageOutput];
+            [self setupLivePreview];
+        }
+    } else {
+        NSLog(@"Error Unable to initialize back camera: %@", error.localizedDescription);
+    }
+}
+
+- (void)setupLivePreview {
+    self.videoPreviewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.captureSession];
+    if(self.videoPreviewLayer) {
+        self.videoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspect;
+        self.videoPreviewLayer.connection.videoOrientation = AVCaptureVideoOrientationPortrait;
+        [self.previewView.layer addSublayer: self.videoPreviewLayer];
+        dispatch_queue_t globalQueue =  dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+        dispatch_async(globalQueue, ^{
+            [self.captureSession startRunning];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.videoPreviewLayer.frame = self.previewView.bounds;
+            });
+        });
+    }
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.captureSession stopRunning];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
